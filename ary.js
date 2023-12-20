81dacf9de85a81eee1bb55772dce9aa43398d610
deepak <deepakvkn1252@gmail.com>
2023-12-20 13:31:23 +0530

    second

---
+++ ary.js
@@ -0,0 +1,18 @@
+
+let a = 'hello wolrd';
+function  a  (s) {
+    console.log(s)
+    let ar=s.split(' ')
+    ar.reverse()
+    console.log(ar)
+    let l=0;
+    let i=0;
+    // while(ar[i]!==' '){
+    //     l++;
+    //     i++;
+    // }
+    //    console.log(l)
+   };
+
+   a();
+
